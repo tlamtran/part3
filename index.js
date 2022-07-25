08361330d5d16037fa451069049d074b7f53d772
Lam-Laptop <lam.tran@aalto.fi>
2022-07-25 16:37:07 +0300

    3.4

--- index.js
+++ index.js
@@ -42,6 +42,12 @@ app.get('/api/persons/:id', (request, response) => {
 
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+    response.status(404).end()
+})
+
 app.get('/info', (request, response) => {
     const numberOfPeople = Math.max(...persons.map( p => p.id))
     const timeReceived = Date()
