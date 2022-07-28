dee4b463125d49e87092cecae3047dcbb2e0ef87
Lam-Laptop <lam.tran@aalto.fi>
2022-07-28 18:45:40 +0300

    3.13-3.14

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-const postBody = morgan.token('type', (req, res) => {
-    req.body
-})
-
+const Person = require('./models/person')
 const app = express()
 
+
+// middleware
 app.use(express.static('build'))
 app.use(morgan(function (tokens, req, res) {
     return [
@@ -48,19 +23,24 @@ app.use(cors())
 
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then( data => {
+        response.json(data)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const maxId = Math.max(...persons.map( p => p.id))
-    if (id > maxId || id <= 0) {
-        response.status(404).end()
-    }
-    else {
-        response.json(persons[(id - 1)])
-    }
+    Person.findById(request.params.id).then( person => {
+        response.json(person)
+    })
+})
 
+app.get('/info', (request, response) => {
+    const numberOfPeople = persons.length
+    const timeReceived = Date()
+    response.send(
+        "<p>Phonebook has info for "+numberOfPeople+" people<p/>" +
+        "<p>"+timeReceived+"<p/>"
+    )
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -74,39 +54,15 @@ app.delete('/api/persons/:id', (request, response) => {
     }
 })
 
-app.get('/info', (request, response) => {
-    const numberOfPeople = persons.length
-    const timeReceived = Date()
-    response.send(
-        "<p>Phonebook has info for "+numberOfPeople+" people<p/>" +
-        "<p>"+timeReceived+"<p/>"
-    )
-})
-
 app.post('/api/persons', (request, response) => {
     const body = request.body
-
-    if (!body.name) {
-        response.status(400).json(
-            {error: "name missing"}
-        )
-    }
-    else if (!body.number) {
-        response.status(400).json(
-            {error: "number missing"}
-        )
-    }
-    else if (persons.map(p => p.name).includes(body.name)) {
-        response.status(400).json(
-            {error: "name already exists"}
-        )
-    }
-    else {
-        const randInt = Math.floor(Math.random() * 1000000)
-        const person = {id: randInt, ...request.body}
-        persons = persons.concat(person)
-        response.json(persons)
-    }
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+    person.save().then( savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 const PORT = process.env.PORT || 3001
